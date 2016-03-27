737a187d068bf79e09aead58653709ff4f9041d0
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-03-27 09:23:00 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,20 @@
 
 import sys
 
+sys.path.append('/home/pi/GitRepos/BiblioPixel')
+
 import bibliopixel
 #causes frame timing information to be output
 bibliopixel.log.setLogLevel(bibliopixel.log.DEBUG)
 
-from bibliopixel.drivers.LPD8806 import *
-#set number of pixels & LED type here
-driver = DriverLPD8806(num = 32)
-
-#load the LEDStrip class
 from bibliopixel.led import *
+from bibliopixel.animation import StripChannelTest
+from bibliopixel.drivers.LPD8806 import *
+
+#create driver for a 12 pixels
+driver = DriverLPD8806(num = 12, c_order = ChannelOrder.GRB)
 led = LEDStrip(driver)
 
+anim = StripChannelTest(led)
+anim.run()
 
