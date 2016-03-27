7ede0bc62e8916d2dea62cf1d685cb3067d870fb
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-03-27 21:17:02 +0000

    Update server.py

--- arp/server.py
+++ arp/server.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="./templates",
+        static_url_path="/static",
+        static_folder="./static")
 
 @app.route('/')
 def home():
-    return 'Hello World!'
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
