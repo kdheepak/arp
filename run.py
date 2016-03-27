e1cb2431f5d03b6fcb139d9321b344abcef8303d
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-03-27 10:32:26 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 
 import sys
+import time
 
 sys.path.append('/home/pi/GitRepos/BiblioPixel')
+sys.path.append('/home/pi/GitRepos/BiblioPixelAnimations')
 
 import bibliopixel
 #causes frame timing information to be output
@@ -11,10 +13,20 @@ from bibliopixel.led import *
 from bibliopixel.animation import StripChannelTest
 from bibliopixel.drivers.LPD8806 import *
 
-#create driver for a 12 pixels
-driver = DriverLPD8806(num = 12, c_order = ChannelOrder.GRB)
+#create driver for a 32 pixels
+driver = DriverLPD8806(num = 32, c_order = ChannelOrder.GRB, use_py_spi=True)
 led = LEDStrip(driver)
 
-anim = StripChannelTest(led)
-anim.run()
+import bibliopixel.colors as colors
+rainbow = [colors.Red, colors.Orange, colors.Yellow, colors.Green, colors.Blue, colors.Purple]
+
+led.masterBrightness = 100
+
+for color in rainbow:
+    time.sleep(.5)
+    led.fill(color)
+    led.update()
+
+led.all_off()
+led.update()
 
